257c1a46ff7a1df9313ad1e6b28f8ce9bcc679d4
Cédric Vigne <cedric.vigne11@gmail.com>
2016-04-04 17:14:35 +0200

    Mise en forme form login

--- php/control/test-pass.php
+++
@@ -1,8 +0,0 @@
-<?php
-    ini_set("display_errors", 1);
-    include_once("../model/base.php");
-    include_once("./AccountControl.php");
-    include_once("../model/AccountModel.php");
-
-    $accountControl = new AccountControl();
-    $accountControl->controlLogin();
